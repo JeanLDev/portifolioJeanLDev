730a770dc7c8a66b95faecb84c7bb9e2dd84e271
Jean lucas <jean__lucas2013@hotmail.com>
2025-12-05 19:28:39 -0300

    base index correto

--- vite.config.js
+++ vite.config.js
@@ -202,7 +202,7 @@ logger.error = (msg, options) => {
 }
 
 export default defineConfig({
-	base: '/portifolioJeanLDev/',
+	base: '/',
 	customLogger: logger,
 	plugins: [
 		...(isDev ? [inlineEditPlugin(), editModeDevPlugin(), iframeRouteRestorationPlugin()] : []),
